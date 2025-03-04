e9f2a96c51edd9cd6de03276e256e2fa5236c052
MathisPFR <mathispetitfr@gmail.com>
2025-03-04 12:27:19 +0100

    controller and model

--- app/Models/Emprunt.php
+++ app/Models/Emprunt.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Emprunt extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'emprunts';
+
+    protected $fillable = [
+        'user_id',
+        'livre_id',
+        'date_emprunt',
+        'date_retour',
+        'statut',
+    ];
 }
